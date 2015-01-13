681c0b1e9ecce74080e15c3ffdbe37a2d88b3eaa
dennyzhang <ztmbird0709@126.com>
2015-01-13 15:35:55 +0800

    bug

---
+++ mission_1/js/main.js
@@ -0,0 +1,11 @@
+$("[name='find']").click(function(){
+    var $search= $(".content").load("./find.html");
+    alert(1);
+})
+
+
+/*
+$(":button.find").on("click",function(){
+    var $search= $(".find").load("./find.html");
+    alert(1);
+})*/
